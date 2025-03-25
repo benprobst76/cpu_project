36689e3de324623463b2efdd98c24971d10cf085
Ben Probst <benprobst76@gmail.com>
2025-03-24 22:00:34 -0400

    delete

--- phase2_/phase2/subtraction.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 10ps
-
-module subtraction(input wire [31:0] RA, input wire [31:0] RB, input wire cin, output wire [31:0] diff, output wire cout);
-	wire [31:0] temp;
-	negate_bits negate(.Ra(RB),.Rz(temp));
-	addition add(.RA(RA), .RB(temp), .cin(cin), .sum(diff), .cout(cout));
-endmodule
-
